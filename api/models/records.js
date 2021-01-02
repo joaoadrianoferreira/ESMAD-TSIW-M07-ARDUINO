c8762062801ce693a0a30420d4cd98d4d0cc9732
João <joaoadrianoferreira@gmail.com>
2021-01-02 14:19:43 +0000

    web

--- api/models/records.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var Record = new mongoose.Schema({
-  timestamp: { type: Date, default: Date.now },
-  value: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('SoundRecords', Record);
