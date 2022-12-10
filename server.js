6b77041a0050f48321aa8035d4a56a83ffe0d70b
João Ferreira <joaoadrianoferreira@gmail.com>
2022-12-10 22:01:02 +0000

    changes

--- server.js
+++ server.js
@@ -9,24 +9,27 @@ var controller = require('./api/controllers/records')
 
 mongoose.connect('mongodb+srv://tsiw:GAa8xvmV3eKrVa8C@cluster0.b0vmz.mongodb.net/TSIW?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
-var client  = mqtt.connect('mqtt://192.168.1.140', { username: 'teste', password: 'teste' })
+var client  = mqtt.connect('mqtts://295a997120e24c60a5207a820b83e5ee.s2.eu.hivemq.cloud', { username: 'joaoadrianoferreira_sub', password: 'dsfsdkjasdfksad892354234' })
  
-client.on('connect', function () {
-  client.subscribe('sound', function (err) {
-    if(err) {
-      console.log(err)
-    }
-  })
+client.on('connect', function (conn) {
+  console.log(conn);
+  client.subscribe('temperature', function (res) {})
 })
  
 client.on('message', function (topic, message) {
+  console.log(message.toString());
   controller.create_record_mqtt(message)
-  client.end()
 })
 
 app.use(express.json());
 app.use(cors()); 
 app.use('/records', records);
+
+app.post('/mqtt', function(req,res) {
+  client.publish('temperature', req.query.temperature); 
+  res.status(200).send('Value Published')
+})
+
 app.listen(port, () => {
   console.log("Server running on port " + port); 
 });
